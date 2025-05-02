69a41314d676834a597cf7238a4f78d37e104b60
FelipePhipe <felipeparragranda@gmail.com>
2025-05-01 19:43:16 -0500

    phyton era chevere

--- src/src_PY/app.py
+++ src/src_PY/app.py
@@ -1,7 +1,29 @@
 from benchmarking import Benchmarking
-from src_PY.benchmarking import Benchmarking
-
+from metodos_ordenamiento import MetodosOrdenamiento
 
 if __name__ == "__main__":
     print("funciona")
-    Benchmarking()
+
+    bench = Benchmarking()
+    metodosO = MetodosOrdenamiento()
+
+    tamanios = [5000, 10000, 15000]
+    resultados = []
+
+    for tam in tamanios:
+        arreglo_base = bench.build_arreglo(tam)  
+
+        metodos_dic = {
+            "burbuja": metodosO.metodo_sort_bubble,
+            "burbuja mejorado": metodosO.metodo_sort_bubble_mejorado,
+            "seleccion": metodosO.metodo_sort_seleccion,
+            "insercion": metodosO.metodo_sort_insercion,
+        }
+
+        for nombre, metodo in metodos_dic.items():
+            time_resultado = bench.medir_tiempo(metodo, arreglo_base)
+            tupla_resultado = (tam, nombre, time_resultado)  
+            resultados.append(tupla_resultado)
+
+    for tam, nombre, tiempo in resultados:
+        print(f"Tamaño: {tam}, nombre método: {nombre}, tiempo: {tiempo:.6f} segundos")
