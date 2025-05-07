086a936541624d6eb3dfa13b95b3cd3dcbc31b71
FelipePhipe <felipeparragranda@gmail.com>
2025-05-06 20:10:48 -0500

    Graficos zzz

--- src/src_PY/app.py
+++ src/src_PY/app.py
@@ -1,5 +1,8 @@
 from benchmarking import Benchmarking
 from metodos_ordenamiento import MetodosOrdenamiento
+import matplotlib.pyplot as plt
+import time
+from datetime import datetime
 
 if __name__ == "__main__":
     print("funciona")
@@ -7,23 +10,60 @@ if __name__ == "__main__":
     bench = Benchmarking()
     metodosO = MetodosOrdenamiento()
 
-    tamanios = [5000, 10000, 15000]
+    tamanios = [500, 1000, 2000]
     resultados = []
 
+    metodos_dic = {
+        "burbuja": metodosO.metodo_sort_bubble,
+        "burbuja_mejorado": metodosO.metodo_sort_bubble_mejorado,
+        "seleccion": metodosO.metodo_sort_seleccion,
+        "shell": sorted
+    }
+
     for tam in tamanios:
-        arreglo_base = bench.build_arreglo(tam)  
-
-        metodos_dic = {
-            "burbuja": metodosO.metodo_sort_bubble,
-            "burbuja mejorado": metodosO.metodo_sort_bubble_mejorado,
-            "seleccion": metodosO.metodo_sort_seleccion,
-            "insercion": metodosO.metodo_sort_insercion,
-        }
-
+        arreglo_base = bench.build_arreglo(tam)
         for nombre, metodo in metodos_dic.items():
-            time_resultado = bench.medir_tiempo(metodo, arreglo_base)
-            tupla_resultado = (tam, nombre, time_resultado)  
-            resultados.append(tupla_resultado)
+            tiempo = bench.medir_tiempo(metodo, arreglo_base)
+            resultados.append((tam, nombre, tiempo))
 
+    tiempos_by_metodo = {nombre: [] for nombre in metodos_dic}
     for tam, nombre, tiempo in resultados:
-        print(f"Tamaño: {tam}, nombre método: {nombre}, tiempo: {tiempo:.6f} segundos")
+        tiempos_by_metodo[nombre].append(tiempo)
+
+  
+    plt.figure(figsize=(12, 6))
+
+    now = datetime.now()
+    plt.suptitle(f"FELIPE PARRA - {now.strftime('%d/%m/%Y %H:%M:%S')}", fontsize=12, weight='bold')
+
+  
+    ax1 = plt.subplot(1, 2, 1)
+    colores = {
+        "burbuja": "blue",
+        "burbuja_mejorado": "orange",
+        "seleccion": "green",
+        "shell": "red"
+    }
+
+    for nombre, tiempos in tiempos_by_metodo.items():
+        ax1.plot(tamanios, tiempos, label=nombre, marker="o", color=colores[nombre])
+
+    ax1.set_title("Comparación de Tiempos de Ejecución de Algoritmos de Ordenamiento")
+    ax1.set_xlabel("Tamaño del arreglo")
+    ax1.set_ylabel("Tiempo de ejecución (segundos)")
+    ax1.grid(True)
+    ax1.legend()
+
+ 
+    ax2 = plt.subplot(1, 2, 2)
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 6, 8, 10]
+    ax2.plot(x, y, label="Línea 1", color="blue")
+    ax2.set_title("Gráfico de Ejemplo con Matplotlib")
+    ax2.set_xlabel("Eje X")
+    ax2.set_ylabel("Eje Y")
+    ax2.grid(True)
+    ax2.legend()
+
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.show()
